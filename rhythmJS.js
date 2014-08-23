8b7e21322ebe08e874a50a17e1129e11f3b6fd67
Wenjie Hu <wenjiehu.web@gmail.com>
2014-08-24 00:15:22 +0800

    add isFunction()

--- rhythmJS.js
+++ rhythmJS.js
@@ -3,5 +3,8 @@
 
 	window.rhythmJS = window.rhythmJS || {};
 
-	
+	// can't be used for determining the function of DOM elements on IE8
+	rhythmJS.isFunction = function(func) {
+		return typeof func === 'function';
+	}
 }(window));
