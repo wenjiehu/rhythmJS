c08e368e7ec4b853e8389193117e4410202b9376
Wenjie Hu <wenjiehu.web@gmail.com>
2014-08-25 16:42:09 +0800

    add function isArray()

--- rhythmJS.js
+++ rhythmJS.js
@@ -3,8 +3,16 @@
 
 	window.rhythmJS = window.rhythmJS || {};
 
-	// can't be used for determining the function of DOM elements on IE8
+	// TODO can't be used for determining the function of DOM elements on IE8
 	rhythmJS.isFunction = function(func) {
 		return typeof func === 'function';
+	}
+
+	rhythmJS.isArray = function(value) {
+		if(typeof Array.isArray === 'function') {
+			return Array.isArray(value);
+		} else {
+			return Object.prototype.toString.call(value) === "[object Array]";
+		}
 	}
 }(window));
