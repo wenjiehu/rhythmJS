77500a208cb819b779bd4f1f072ee2bd782d3f18
Wenjie Hu <wenjiehu.web@gmail.com>
2014-08-27 13:42:42 +0800

    add addEvent() and removeEvent()

--- rhythmJS.js
+++ rhythmJS.js
@@ -1,18 +1,42 @@
 (function(window) {
-	'use strict';
+    'use strict';
 
-	window.rhythmJS = window.rhythmJS || {};
+    window.rhythmJS = window.rhythmJS || {};
 
-	// TODO can't be used for determining the function of DOM elements on IE8
-	rhythmJS.isFunction = function(func) {
-		return typeof func === 'function';
-	}
+    // Nicholas C. Zakas, Maintainable Javascript
+    // TODO can't be used for determining the function of DOM elements on IE8
+    rhythmJS.isFunction = function(func) {
+        return typeof func === 'function';
+    }
 
-	rhythmJS.isArray = function(value) {
-		if(typeof Array.isArray === 'function') {
-			return Array.isArray(value);
-		} else {
-			return Object.prototype.toString.call(value) === "[object Array]";
-		}
-	}
+    // Nicholas C. Zakas, Maintainable Javascript
+    rhythmJS.isArray = function(value) {
+        if (typeof Array.isArray === 'function') {
+            return Array.isArray(value);
+        } else {
+            return Object.prototype.toString.call(value) === "[object Array]";
+        }
+    }
+
+    // John Resig, http://ejohn.org/projects/flexible-javascript-events/
+    rhythmJS.addEvent = function(obj, type, fn) {
+        if (obj.attachEvent) {
+            obj['e' + type + fn] = fn;
+            obj[type + fn] = function() {
+                obj['e' + type + fn](window.event);
+            }
+            obj.attachEvent('on' + type, obj[type + fn]);
+        } else {
+            obj.addEventListener(type, fn, false);
+        }
+    }
+
+    rhythmJS.removeEvent = function(obj, type, fn) {
+        if (obj.detachEvent) {
+            obj.detachEvent('on' + type, obj[type + fn]);
+            obj[type + fn] = null;
+        } else {
+            obj.removeEventListener(type, fn, false);
+        }
+    }
 }(window));
